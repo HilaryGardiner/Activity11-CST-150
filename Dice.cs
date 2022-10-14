a2e5e6bb8c93c6f2595bce4dfc44dc8e801745ab
HilaryGardiner <114051999+HilaryGardiner@users.noreply.github.com>
2022-10-14 04:51:15 -0700

    Add files via upload

---
+++ Dice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity11App
+{
+    internal class Dice
+    {
+        private int numberOfSidesOfTheDie;
+
+        // Generates random numbers
+        private static Random randomNumber = new Random();
+
+        public Dice(int numberOfSidesOfTheDie)
+        {
+
+            if (numberOfSidesOfTheDie < 4 || numberOfSidesOfTheDie > 20) // takes integer between 4 and 20
+            {
+                throw new ArgumentOutOfRangeException("Should be in range [4, 20] !");
+            }
+
+            this.numberOfSidesOfTheDie = numberOfSidesOfTheDie;
+        }
+
+        public int rollDie() // rollDie method
+        {
+            return randomNumber.Next(1, numberOfSidesOfTheDie + 1); //returns random number when the die is rolled
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Activity11App
+{
+    public class DieTest
+    {
+        public static void Main(string[] args) 
+        {
+            Dice die1 = new Dice(6); // creates die1 object
+            Dice die2 = new Dice(6); // creates die2 object
+
+            int die1Roll, die2Roll; //variables to store dieRoll value
+            int theNumberOfRolls = 0; //variable to count numberOfTimesDieRolled
+
+            do
+            {
+
+                die1Roll = die1.rollDie(); // assigns the number to die1Roll
+                die2Roll = die2.rollDie(); // assigns the number to die2Roll
+
+                Console.WriteLine("Rolled [Dice 1: " + die1Roll + ", Dice 2: " + die2Roll + "]"); //prints to the console
+
+                ++theNumberOfRolls; // increments each time
+
+            } while (die1Roll != 1 || die2Roll != 1); //  while die1Roll is not equal to 1 or die2Roll is not equal to 1
+
+            Console.WriteLine("\nIt took " + theNumberOfRolls + " rolls to get snake eyes!"); //prints to the console
+        }
+    }
+}
